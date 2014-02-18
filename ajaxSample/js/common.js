b4a8f887e0b1d1b69c890919ed7e83a1ef31d240
nchime <nchime@gmail.com>
2014-02-18 20:07:09 +0900

    start
    start

---
+++ ajaxSample/js/common.js
@@ -0,0 +1,6 @@
+/**
+ * 
+ * 공통 참조 모듈 
+ */
+var parseHeaders = {"X-Parse-Application-Id":"RSU6lifgLRciaTn60UdVU3x4STG3IHRAqgtf9aFI","X-Parse-REST-API-Key":"5EBGWjMdh4ThyDhkM0V5coIB6yqUUjv7Xjr13lpm"}; 
+var userSessionToken = ""; 
